7a41ac205ac6e62e1c663afb0a7fb5db9a99f0b8
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-03-23 22:50:26 -0400

    added some js
    added some js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const axios = require('axios');
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-	res.send('haha');
+	res.sendFile(__dirname + '/signup.html');
 });
 
-app.listen(3000, () => {
-	console.log('Server is running at 3000');
+app.post('/', (req, res) => {
+	const fName = req.body.fName;
+	const lName = req.body.lName;
+	const email = req.body.email;
+	console.log(fName, lName, email);
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at ${port}`);
 });
